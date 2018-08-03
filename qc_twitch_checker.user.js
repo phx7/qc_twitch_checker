205d7b4e24e5698e7bd54435dad5d5a2eb2789bf
AndreyZ <Jukovskii@sugartalk.eu>
2018-08-04 01:20:32 +0300

    version

--- qc_twitch_checker.user.js
+++ qc_twitch_checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QC twitch stream checker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  hai
 // @author       @yolanda_becool
 // @match        https://www.twitch.tv/*
